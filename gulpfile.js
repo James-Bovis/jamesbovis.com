ad1bbba9a566c360088cc4e55702a3bca71b69fd
James Bovis <jamesbovis@googlemail.com>
2017-07-30 19:11:44 +0100

    30/07/2017 - Added support for Gulp Includes: - SASS Compliling - Browser Re-load

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp');
+
+// Requires the gulp-sass plugin
+var sass = require('gulp-sass');
+
+// Requires Live Reload
+var browserSync = require('browser-sync').create();
+
+gulp.task('sass', function(){
+  return gulp.src('stylesheets/main.scss')
+    .pipe(sass()) // Using gulp-sass
+    .pipe(gulp.dest('stylesheets/'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: ''
+    },
+  })
+})
+
+
+// Gulp watch syntax
+gulp.task('watch', ['browserSync', 'sass'], function (){
+  gulp.watch('stylesheets/**/*.scss', ['sass']); 
+  // Other watchers
+  gulp.watch('**/*.html', browserSync.reload);
+})
