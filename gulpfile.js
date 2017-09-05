36cf70ee35eee5fb96cf05edfb130f013ce49762
James Bovis <jamesbovis@googlemail.com>
2017-09-05 22:42:02 +0100

    Add comments or better understanding of Gulp. Compresses CSS

--- gulpfile.js
+++ gulpfile.js
@@ -6,16 +6,17 @@ var sass = require('gulp-sass');
 // Requires Live Reload
 var browserSync = require('browser-sync').create();
 
+// Compiles the sass code into css
 gulp.task('sass', function(){
   return gulp.src('stylesheets/main.scss')
-    .pipe(sass()) // Using gulp-sass
+    .pipe(sass({outputStyle: 'compressed'})) // Using gulp-sass - outputs compressed file
     .pipe(gulp.dest('stylesheets/'))
     .pipe(browserSync.reload({
       stream: true
     }))
 });
 
-
+// Runs browsersync on root folder
 gulp.task('browserSync', function() {
   browserSync.init({
     server: {
